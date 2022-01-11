a31e309da4d3fd948d28fbfe2b36e5c6d5d39e1e
anyamri <96358029+anyamri@users.noreply.github.com>
2022-01-10 19:27:27 -0600

    Update US Medical Insurance Costs.py

--- US Medical Insurance Costs.py
+++ US Medical Insurance Costs.py
@@ -28,11 +28,9 @@ with open("C:\\Users\\ayamr\\Documents\\Coding\\Python portfolio\\Insurance.csv"
 # Save data into a list of dictionaries
     for row in reader2:
         data = [row for row in reader2]
-# Function to determine least expensive region
+# Function to determine least expensive region by average cost of insurance
 def least_expensive(r, c):
     charges_float = [float(x) for x in c]
-    cheapest_region = ''
-    cheapest_amount = 0
     duplicate_dict={} 
     for i in r:
         duplicate_dict[i]=r.count(i)
@@ -41,12 +39,12 @@ def least_expensive(r, c):
         d[k] += v
     region_average = {k: d[k]/duplicate_dict[k] for k in d.keys() & duplicate_dict}
     region_average_sorted = {k: v for k, v in sorted(region_average.items(), key=lambda item: item[1])}
-
-    print(duplicate_dict)
-    print(d)
-    print(region_average_sorted)
-    # return unique_regions
-print(least_expensive(region, charges))
+    cheapest_region = list(region_average_sorted.keys())[0]
+    cheapest_amount = list(region_average_sorted.values())[0]
+    pricey_region = list(region_average_sorted.keys())[-1]
+    pricey_amount = list(region_average_sorted.values())[-1]
+    return ("The cheapest region for health insurance is the " + cheapest_region + " region with an average cost of $" + str(cheapest_amount) + ". The most expensive region is the " + pricey_region + " region with an average cost of $" + str(pricey_amount) +".")
+# print(least_expensive(region, charges))
 
 
 
